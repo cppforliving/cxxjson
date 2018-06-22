893bfe744c004877d1b1a647cde535f4a47985eb
Karol Wozniak <karol.wozniak@email.com>
2018-06-22 23:23:29 +0200

    add more ctor benchmarks

--- benchmark/json_ctors.cpp
+++ benchmark/json_ctors.cpp
@@ -20,8 +20,38 @@ static void cxx_json_integer_ctor(benchmark::State& state)
 }
 BENCHMARK(cxx_json_integer_ctor);
 
+static void cxx_json_boolean_ctor(benchmark::State& state)
+{
+  for (auto _ : state) benchmark::DoNotOptimize(cxx::json(true));
+}
+BENCHMARK(cxx_json_boolean_ctor);
+
+static void cxx_json_null_ctor(benchmark::State& state)
+{
+  for (auto _ : state) benchmark::DoNotOptimize(cxx::json(cxx::null));
+}
+BENCHMARK(cxx_json_null_ctor);
+
+static void cxx_json_double_ctor(benchmark::State& state)
+{
+  for (auto _ : state) benchmark::DoNotOptimize(cxx::json(3.14));
+}
+BENCHMARK(cxx_json_double_ctor);
+
 static void cxx_json_string_ctor(benchmark::State& state)
 {
-  for (auto _ : state) benchmark::DoNotOptimize(cxx::json("lorem"));
+  for (auto _ : state) benchmark::DoNotOptimize(cxx::json(std::string()));
 }
 BENCHMARK(cxx_json_string_ctor);
+
+static void cxx_json_array_ctor(benchmark::State& state)
+{
+  for (auto _ : state) benchmark::DoNotOptimize(cxx::json(cxx::array()));
+}
+BENCHMARK(cxx_json_array_ctor);
+
+static void cxx_json_document_ctor(benchmark::State& state)
+{
+  for (auto _ : state) benchmark::DoNotOptimize(cxx::json(cxx::document()));
+}
+BENCHMARK(cxx_json_document_ctor);
