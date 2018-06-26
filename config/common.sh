780517ee9a8c49203557b502039b12a29959d26f
Karol Wozniak <karol.wozniak@email.com>
2018-06-26 08:58:14 +0200

    add clang and libc++ to image

--- config/common.sh
+++ config/common.sh
@@ -1,3 +1,3 @@
-export CXXFLAGS="-Wall -Wconversion -Werror -Wextra -Wshadow -Wunused -pedantic -pedantic-errors -fno-rtti -fPIC -std=c++17"
-export LINKFLAGS=""
-export DEFINES=""
+export CXXFLAGS="${CXXFLAGS} -Wall -Wconversion -Werror -Wextra -Wshadow -Wunused -pedantic -pedantic-errors -fno-rtti -fPIC -std=c++17"
+export LINKFLAGS="${LINKFLAGS}"
+export DEFINES="${DEFINES}"
