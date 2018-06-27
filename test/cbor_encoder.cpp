ad07a2fd06832b9c98e9c537348123132b3b58fa
Karol Wozniak <karol.wozniak@email.com>
2018-06-27 15:07:11 +0200

    hex stream string literal

--- test/cbor_encoder.cpp
+++ test/cbor_encoder.cpp
@@ -7,6 +7,43 @@ using namespace cxx::literals;
 using cbor = cxx::cbor;
 using stream = cbor::byte_stream;
 
+namespace
+{
+  template <char h, char l, char... rest>
+  void byte_literal(stream& bytes)
+  {
+    static_assert((h >= 'a' && h <= 'f') || (h >= '0' && h <= '9'));
+    static_assert((l >= 'a' && l <= 'f') || (l >= '0' && l <= '9'));
+    char h_base = 0;
+    char l_base = 0;
+    if
+      constexpr(h >= 'a' && h <= 'f') { h_base = 'a' - 10; }
+    else
+    {
+      h_base = '0';
+    }
+    if
+      constexpr(l >= 'a' && l <= 'f') { l_base = 'a' - 10; }
+    else
+    {
+      l_base = '0';
+    }
+    bytes.emplace_back((stream::value_type(h - h_base) << 4) | stream::value_type(l - l_base));
+    if
+      constexpr(sizeof...(rest) > 1) byte_literal<rest...>(bytes);
+  }
+
+  template <typename T, T... args>
+  stream operator"" _hex()
+  {
+    static_assert(sizeof...(args) % 2 == 0);
+    stream bytes;
+    bytes.reserve(sizeof...(args) / 2);
+    byte_literal<args...>(bytes);
+    return bytes;
+  }
+}
+
 TEST_CASE("cbor sets correct initial byte for integers")
 {
   SECTION("positive")
@@ -49,25 +86,23 @@ TEST_CASE("cbor can encode positive integers")
   }
   SECTION("that fit on one additional byte")
   {
-    REQUIRE(cbor::encode(0x18) == stream({0x18, 0x18}));
-    REQUIRE(cbor::encode(0xff) == stream({0x18, 0xff}));
+    REQUIRE(cbor::encode(0x18) == "1818"_hex);
+    REQUIRE(cbor::encode(0xff) == "18ff"_hex);
   }
   SECTION("that fit on two additional bytes")
   {
-    REQUIRE(cbor::encode(0x100) == stream({0x19, 0x01, 0x00}));
-    REQUIRE(cbor::encode(0xfffe) == stream({0x19, 0xff, 0xfe}));
+    REQUIRE(cbor::encode(0x100) == "190100"_hex);
+    REQUIRE(cbor::encode(0xfffe) == "19fffe"_hex);
   }
   SECTION("that fit on four additional bytes")
   {
-    REQUIRE(cbor::encode(0x10000) == stream({0x1a, 0x00, 0x01, 0x00, 0x00}));
-    REQUIRE(cbor::encode(0x12345678) == stream({0x1a, 0x12, 0x34, 0x56, 0x78}));
+    REQUIRE(cbor::encode(0x10000) == "1a00010000"_hex);
+    REQUIRE(cbor::encode(0x12345678) == "1a12345678"_hex);
   }
   SECTION("that fir on eight additional bytes")
   {
-    REQUIRE(cbor::encode(0x100000000) ==
-            stream({0x1b, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00}));
-    REQUIRE(cbor::encode(0x1122334455667788) ==
-            stream({0x1b, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88}));
+    REQUIRE(cbor::encode(0x100000000) == "1b0000000100000000"_hex);
+    REQUIRE(cbor::encode(0x1122334455667788) == "1b1122334455667788"_hex);
   }
 }
 
@@ -83,48 +118,46 @@ TEST_CASE("cbor can encode negative integers")
   }
   SECTION("that fit on one additional byte")
   {
-    REQUIRE(cbor::encode(-0x19) == stream({0x38, 0x18}));
-    REQUIRE(cbor::encode(-0x100) == stream({0x38, 0xff}));
+    REQUIRE(cbor::encode(-0x19) == "3818"_hex);
+    REQUIRE(cbor::encode(-0x100) == "38ff"_hex);
   }
   SECTION("that fit on two additional bytes")
   {
-    REQUIRE(cbor::encode(-0x101) == stream({0x39, 0x01, 0x00}));
-    REQUIRE(cbor::encode(-0xffff) == stream({0x39, 0xff, 0xfe}));
+    REQUIRE(cbor::encode(-0x101) == "390100"_hex);
+    REQUIRE(cbor::encode(-0xffff) == "39fffe"_hex);
   }
   SECTION("that fit on four additional bytes")
   {
-    REQUIRE(cbor::encode(-0x10001) == stream({0x3a, 0x00, 0x01, 0x00, 0x00}));
-    REQUIRE(cbor::encode(-0x12345679) == stream({0x3a, 0x12, 0x34, 0x56, 0x78}));
+    REQUIRE(cbor::encode(-0x10001) == "3a00010000"_hex);
+    REQUIRE(cbor::encode(-0x12345679) == "3a12345678"_hex);
   }
   SECTION("that fir on eight additional bytes")
   {
-    REQUIRE(cbor::encode(-0x100000001) ==
-            stream({0x3b, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00}));
-    REQUIRE(cbor::encode(-0x1122334455667789) ==
-            stream({0x3b, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88}));
+    REQUIRE(cbor::encode(-0x100000001) == "3b0000000100000000"_hex);
+    REQUIRE(cbor::encode(-0x1122334455667789) == "3b1122334455667788"_hex);
   }
 }
 
 TEST_CASE("cbor can encode null")
 {
-  REQUIRE(cbor::encode(cxx::null) == stream({0xf6}));
+  REQUIRE(cbor::encode(cxx::null) == "f6"_hex);
 }
 
 TEST_CASE("cbor can encode booleans")
 {
-  REQUIRE(cbor::encode(false) == stream({0xf4}));
-  REQUIRE(cbor::encode(true) == stream({0xf5}));
+  REQUIRE(cbor::encode(false) == "f4"_hex);
+  REQUIRE(cbor::encode(true) == "f5"_hex);
 }
 
 TEST_CASE("cbor can encode strings")
 {
-  REQUIRE(cbor::encode("") == stream({0x60}));
-  REQUIRE(cbor::encode("a") == stream({0x61, 0x61}));
-  REQUIRE(cbor::encode("IETF") == stream({0x64, 0x49, 0x45, 0x54, 0x46}));
-  REQUIRE(cbor::encode("\"\\") == stream({0x62, 0x22, 0x5c}));
-  REQUIRE(cbor::encode("ü") == stream({0x62, 0xc3, 0xbc}));
-  REQUIRE(cbor::encode("水") == stream({0x63, 0xe6, 0xb0, 0xb4}));
-  REQUIRE(cbor::encode("𐅑") == stream({0x64, 0xf0, 0x90, 0x85, 0x91}));
+  REQUIRE(cbor::encode("") == "60"_hex);
+  REQUIRE(cbor::encode("a") == "6161"_hex);
+  REQUIRE(cbor::encode("IETF") == "6449455446"_hex);
+  REQUIRE(cbor::encode("\"\\") == "62225c"_hex);
+  REQUIRE(cbor::encode("ü") == "62c3bc"_hex);
+  REQUIRE(cbor::encode("水") == "63e6b0b4"_hex);
+  REQUIRE(cbor::encode("𐅑") == "64f0908591"_hex);
   REQUIRE(cbor::encode("lorem") == stream({0x65, 'l', 'o', 'r', 'e', 'm'}));
   REQUIRE(cbor::encode("ipsum dolor sit amet consectetur") ==
           stream({0x78, 0x20, 'i', 'p', 's', 'u', 'm', ' ', 'd', 'o', 'l', 'o',
@@ -134,21 +167,21 @@ TEST_CASE("cbor can encode strings")
 
 TEST_CASE("cbor can encode double")
 {
-  REQUIRE(cbor::encode(0.0) == stream({0xfb, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}));
-  REQUIRE(cbor::encode(1.0) == stream({0xfb, 0x3f, 0xf0, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}));
-  REQUIRE(cbor::encode(1.1) == stream({0xfb, 0x3f, 0xf1, 0x99, 0x99, 0x99, 0x99, 0x99, 0x9a}));
+  REQUIRE(cbor::encode(0.0) == "fb0000000000000000"_hex);
+  REQUIRE(cbor::encode(1.0) == "fb3ff0000000000000"_hex);
+  REQUIRE(cbor::encode(1.1) == "fb3ff199999999999a"_hex);
 }
 
 TEST_CASE("cbor can encode arrays")
 {
-  REQUIRE(cbor::encode(cxx::array()) == stream({0x80}));
-  REQUIRE(cbor::encode({7}) == stream({0x81, 0x07}));
+  REQUIRE(cbor::encode(cxx::array()) == "80"_hex);
+  REQUIRE(cbor::encode({7}) == "8107"_hex);
   REQUIRE(cbor::encode({7, "lorem"}) == stream({0x82, 0x07, 0x65, 'l', 'o', 'r', 'e', 'm'}));
 }
 
 TEST_CASE("cbor can encode documents")
 {
-  REQUIRE(cbor::encode(cxx::document()) == stream({0xa0}));
+  REQUIRE(cbor::encode(cxx::document()) == "a0"_hex);
   REQUIRE(cbor::encode({{"lorem"_key, 42}, {"ipsum"_key, "dolor"}}) ==
           stream({0xa2, 0x65, 'i',  'p', 's', 'u', 'm', 0x65, 'd',  'o', 'l',
                   'o',  'r',  0x65, 'l', 'o', 'r', 'e', 'm',  0x18, 0x2a}));
