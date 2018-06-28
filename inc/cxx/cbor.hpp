04b1a8dca0800b97a5dd0223b977955779398067
Karol Wozniak <karol.wozniak@email.com>
2018-06-28 15:49:29 +0200

    can decode single additional byte integer

--- inc/cxx/cbor.hpp
+++ inc/cxx/cbor.hpp
@@ -17,6 +17,10 @@ namespace cxx
       using error::error;
       virtual ~unsupported() = default;
     };
+    struct data_error : virtual error {
+      using error::error;
+      virtual ~data_error() = default;
+    };
     using byte_view = std::basic_string_view<json::byte_stream::value_type>;
     static json::byte_stream encode(json const&) noexcept;
     static std::pair<json, byte_view> decode(json::byte_stream const&);
